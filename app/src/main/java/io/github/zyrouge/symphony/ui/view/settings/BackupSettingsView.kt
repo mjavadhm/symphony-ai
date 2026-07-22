c0efca1854b9293076334f2f709c4ec0d8068b2a
Javad <you@example.com>
2026-07-23 02:23:45 +0330

    fix

--- app/src/main/java/io/github/zyrouge/symphony/ui/view/settings/BackupSettingsView.kt
+++ app/src/main/java/io/github/zyrouge/symphony/ui/view/settings/BackupSettingsView.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
@@ -29,8 +30,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.layout.Arrangement
+
 import dev.chrisbanes.haze.hazeSource
 import io.github.zyrouge.symphony.services.BackupManager
 import io.github.zyrouge.symphony.ui.components.GlassChip
